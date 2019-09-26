7249612668259b12e50cbfd0712765cd56bebd36
Juan Mariñez <juan.dsmarinez@gmail.com>
2019-09-26 00:01:24 -0400

    [ADD] Drawer and body text and buttons sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,61 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(ScheduleApp());
+  runApp(ControlApp());
 }
 
-class ScheduleApp extends StatelessWidget {
+class ControlApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Schedule App"),
+          title: Text("Control App"),
+          backgroundColor: Colors.red,
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text("Control"),
+                decoration: BoxDecoration(color: Colors.red),
+              ),
+              ListTile(
+                title: Text("Asistencia"),
+                onTap: null,
+              ),
+              ListTile(
+                title: Text("Recursos Humanos"),
+                onTap: null,
+              )
+            ],
+          ),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Text("Estado:"),
+              RaisedButton(
+                child: Text("Asistio"),
+                onPressed: () {
+                  print("Asistio!");
+                },
+              ),
+              RaisedButton(
+                child: Text("Llego tarde"),
+                onPressed: () {
+                  print("Llego tarde!");
+                },
+              ),
+              RaisedButton(
+                child: Text("No llego"),
+                onPressed: () {
+                  print("No llego!");
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
